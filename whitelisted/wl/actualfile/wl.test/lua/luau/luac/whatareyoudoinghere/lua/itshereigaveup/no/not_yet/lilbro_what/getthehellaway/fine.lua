331f0587ac7b3137980c88155755acacba041d13
TBXploits <zol9341v@gmail.com>
2024-09-20 09:17:52 +0700

    Create fine.lua

---
+++ whitelisted/wl/actualfile/wl.test/lua/luau/luac/whatareyoudoinghere/lua/itshereigaveup/no/not_yet/lilbro_what/getthehellaway/fine.lua
@@ -0,0 +1,13 @@
+return {
+    ["takizari_13"] = true,
+    ["TBXploits"] = true,
+    ["BanS933dRun"] = true,
+    ["Kraken6024"] = true,
+    ["xodpc"] = true,
+    ["T_itle2004"] = true,
+    ["T_itle2008"] = true,
+    ["MakeItEasy"] = true,
+    ["BidoSkinsYT"] = true,
+    ["TBWareOfficial"] = true,
+    ["TBXenOfficial"] = true,
+}
